88ba4519a21923960c83017f575bd25fbd4550e2
BISWAJIT ROUTRAY <biswajitroutray58@gmail.com>
2025-06-25 17:23:59 +0530

    updated

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,8 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  title: 'CodeX Academy',
+  description: 'Welcome to the My software Academy',
 }
 
 export default function RootLayout({
